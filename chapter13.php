b4cf69ce21e00fe2e055684d6e308f1c20e96cd3
Aravind <aravindpulipra@gmail.com>
2023-09-18 15:48:42 +0530

    Finished Chapter13.php. Functions

---
+++ chapter13.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functions</title>
+</head>
+<body>
+    <?php
+        function sum($n1, $n2){
+            return $n1*$n2;
+        }
+         echo sum(10, 50);
+    ?>
+    
+</body>
+</html>
