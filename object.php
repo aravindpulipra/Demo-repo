8e493c2ef95dc4a3c6d0677a359080904c501adb
Aravind <aravindpulipra@gmail.com>
2023-09-22 10:39:47 +0530

    Finished sql connect

---
+++ object.php
@@ -0,0 +1,22 @@
+<?php
+class student{
+    private $name;
+    private $roll_number;
+    public function setName($n){
+        $this->name = $n;
+
+    }
+    public function setRollNumber($r){
+        $this->roll_number = $r;
+
+    }
+    public function getName(){
+        return $this->name;
+
+    }
+    public function getRollNumber(){
+        
+
+    }
+
+}
